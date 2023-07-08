78093323588753212385c7584c3f05392b08436e
Huge & Bulk <138213068+hugeandbulk@users.noreply.github.com>
2023-07-08 23:29:52 +0530

    Update Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,1 +1,39 @@
-Gruntfile.js
+#!/usr/bin/env node
+
+
+/*!
+ * Seed-Build-System®
+ * A Xpert Build System — http://seed.sequomics.com/.
+ * ____________________________________________________________________
+ *
+ * Grunt, http://gruntjs.com/.
+ * The JavaScript Task Runner.
+ * ____________________________________________________________________
+ *
+ * Architecture and Code Handcrafted by Prabhat Kumar.
+ * Architectuur en Code handgemaakt door Prabhat Kumar.
+ * @author    : Prabhat Kumar [http://prabhatkumar.org/].
+ * @copyright : Prabhat Kumar [http://prabhatkumar.org/].
+ * @copyright : Sequømics Corporation [http://sequomics.com/].
+ * ____________________________________________________________________
+ *
+ * @date      : 08 July 2023
+ * @license   : Apache License
+ * @require   : Node.js®
+ * @require   : NPM
+ * @require   : grunt-cli
+ * @build     : Seed™: Örebro
+ *              |---- A Sequømics Build Product, http://sequomics.com/.
+ * ____________________________________________________________________
+ *
+ * --/The Heart of Build + Test System/-- of "Seed-Build-System®".
+ * ____________________________________________________________________
+ */
+
+
+// # Usage: $ node -v
+// # Usage: $ npm -v
+// # Usage: $ grunt -version
+
+// Invoking strict mode.
+"use strict";
